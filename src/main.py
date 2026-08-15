50d39f20f6178f5ab48a96941db9e3d494ee7478
João Vitor Dias <joaovitordiasc.25@gmail.com>
2026-08-15 10:09:29 -0300

    feat: Crate main.py

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+#from Ipython.display import display, Markdown
+
+#from config import 
+
+pd.set_option('display.float_format', lambda x: '%.4f' % x)
