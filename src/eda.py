9b48d466f901e0a99878b628f36c8a567b9e878b
João Vitor Dias <joaovitordiasc.25@gmail.com>
2026-08-15 13:30:20 -0300

    feat: Add pie graphic 'Taxa de Churn Geral'

--- src/eda.py
+++ src/eda.py
@@ -1,7 +1,13 @@
+from charts import grafico_pie
+
 def eda(df_churn):
     # Informações do dataframe
     print(f"\n ----- Informações do dataframe: ----- \n")
     df_churn.info()
     print(f"\n ----- Resumo Estatístico: ----- \n\n {df_churn.describe()}")
     print(f"\n ----- Resumo Estatístico das Variáveis Categóricas: ----- \n\n {df_churn.describe(include='object')}")
+    
+    churn_count = df_churn['Churn'].value_counts().rename(index = {1: 'Sim', 0: 'Não'})
+    
+    grafico_pie('Taxa de Churn Geral',churn_count)
     
