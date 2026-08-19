ec3a8143bba1abc645ee9fd59a8352507b315707
João Vitor Dias <joaovitordiasc.25@gmail.com>
2026-08-18 21:56:19 -0300

    fix: constant PATH fix directory

--- src/config.py
+++ src/config.py
@@ -1,3 +1,16 @@
 #/data_generator.py
 
 SEED = 42
+
+#/main.py
+NUM_CLIENTES = 2000
+
+#/charts
+FIGURE_SIZE = (12, 6)
+TITLE_FONT_SIZE = 14
+AZUL = '#636EFA'
+LARANJA = '#EF553B'
+PATH = "image/"
+DPI = 300
+
+
